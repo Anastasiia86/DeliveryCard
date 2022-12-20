e957dfe5a515849d2b87363d4e38cc7b9d0f2e84
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2022-12-20 21:20:05 +0300

    update files

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -33,7 +33,7 @@ public class DeliveryCardTest {
                         Duration.ofSeconds(15));
     }
 
-    @Test   //№2
+    @Test
     public void shouldSuccessfulFormSubmissionWithComplexElements() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Ха");
@@ -56,28 +56,5 @@ public class DeliveryCardTest {
                         Duration.ofSeconds(15));
     }
 
-
-//class DeliveryCardTest {
-    //@Test
-    // void shouldRegisterByAccountNumberDOMModification() {
-    //  open("http://localhost:9999");
-    // $$(".tab-item").find(exactText("По номеру счёта")).click();
-    // $("[name='number']").setValue("4055 0100 0123 4613 8564");
-    //  $("[name='phone']").setValue("+792000000000");
-    //  $$("button").find(exactText("Продолжить")).click();
-    //   $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
-    //  $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
-    // }
-
-    //  @Test
-    //  void shouldRegisterByAccountNumberVisibilityChange() {
-    //    open("http://localhost:9999");
-    //   $$(".tab-item").find(exactText("По номеру счёта")).click();
-    //    $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
-    //   $$("[name='phone']").last().setValue("+792000000000");
-    //  $$("button").find(exactText("Продолжить")).click();
-    //  $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(5));
-    //  $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
-    // }
 }
 
